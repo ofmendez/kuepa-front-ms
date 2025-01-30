1f820da50fea43c6bef0b7acf705ee80a0ceab2a
Fabian Mendez <ofmendez@gmail.com>
2025-01-30 04:08:45 -0500

    fix: enhance WebSocket message handling to ignore 'ping' messages and log received message type

--- src/hooks/useChatWebSocket.js
+++ src/hooks/useChatWebSocket.js
@@ -17,6 +17,10 @@ const useChatWebSocket = () => {
     };
 
     ws.current.onmessage = (event) => {
+      console.log('WebSocket message received:', typeof event.data);
+      const msj = JSON.parse(event.data);
+      if (msj.type === 'ping')
+        return;
       const data = new Message(JSON.parse(event.data));
       if (data.id !== user._id)
         setMessages((prevMessages) => [...prevMessages, data]);
